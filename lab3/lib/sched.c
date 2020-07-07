5e5b2c1e563b64397abdd0b99539f69177bf4041
Jingzhuo-Liang <53360125+Jingzhuo-Liang@users.noreply.github.com>
2020-07-07 21:25:54 +0800

    Add files via upload

---
+++ lab3/lib/sched.c
@@ -0,0 +1,56 @@
+#include <env.h>
+#include <pmap.h>
+#include <printf.h>
+
+/* Overview:
+ *  Implement simple round-robin scheduling.
+ *
+ *
+ * Hints:
+ *  1. The variable which is for counting should be defined as 'static'.
+ *  2. Use variable 'env_sched_list', which is a pointer array.
+ *  3. CANNOT use `return` statement!
+ */
+/*** exercise 3.14 ***/
+void sched_yield(void)
+{
+    static int count = 1; // remaining time slices of current env
+    static int point = 0; // current env_sched_list index
+    struct Env *e; 
+    /*  hint:
+     *  1. if (count==0), insert `e` into `env_sched_list[1-point]`
+     *     using LIST_REMOVE and LIST_INSERT_TAIL.
+     *  2. if (env_sched_list[point] is empty), point = 1 - point;
+     *     then search through `env_sched_list[point]` for a runnable env `e`, 
+     *     and set count = e->env_pri
+     *  3. count--
+     *  4. env_run()
+     *
+     *  functions or macros below may be used (not all):
+     *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
+     */
+	count--;
+	//printf("begin\n");
+	if (count == 0|| curenv ==NULL) {
+		if (curenv!=NULL)
+			//LIST_REMOVE(curenv,env_link);
+			LIST_INSERT_TAIL(&env_sched_list[1-point],curenv,env_link);
+		if (LIST_EMPTY(&env_sched_list[point])){
+			//printf("111\n");
+			point = 1 - point;
+		}
+		if (LIST_EMPTY(&env_sched_list[point]))
+			panic("two lists are all empty");
+		for (e = LIST_FIRST(&env_sched_list[point]);e!=NULL;e = LIST_NEXT(e,env_link)){
+		if (e->env_status ==ENV_RUNNABLE){
+			//printf("222\n");
+			LIST_REMOVE(e,env_link);
+			count = e->env_pri;
+			env_run(e);
+			break;
+			}
+		}
+	}
+	//printf("333\n");
+	env_run(curenv);
+}
